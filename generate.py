158db7875f1d8915c2e6c5ef2f0bc4a15303bfe2
Alcino Cunha <alcino@di.uminho.pt>
2025-10-29 10:30:15 +0000

    add to remove alloy6 specs manually because of before

--- generate.py
+++ generate.py
@@ -1,20 +1,20 @@
 import json
 import os
 
-
-sizes = list(range(4, 33, 4))
+# the min number of wrong specifications per requirement is 29
+sizes = list(range(4, 28, 4))
 
 with open('dataset.json', 'r') as f:
     dataset = json.load(f)
     for example in dataset[:3]:
         base_model = example['model']
         for req in example['requirements']:
-            all_specifications = [spec for spec in [req["oracle"]] + req["erroneous"]]
-            alloy6_keywords = ["'","before"]
-            alloy5_specifications = [spec for spec in all_specifications if not any(c in spec for c in alloy6_keywords)]
-            specifications = {}
+            all_specifications = [req["oracle"]] + req["erroneous"]
             for n in sizes:
-                specifications = alloy5_specifications[:n]
+                if n > len(all_specifications):
+                    print(f"Skipping generation for {example['id']} {req['pred']} size {n} since only {len(all_specifications)} specifications are available.")
+                    continue
+                specifications = all_specifications[:n]
                 model = base_model
                 for i,spec in enumerate(specifications):
                     model += f"\npred S{i} {{ {spec} }}\n"
